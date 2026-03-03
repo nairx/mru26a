18d203ee3369ff20c92d96a00341e29b8ddd5133
nairx <nairxprv@gmail.com>
2026-03-03 15:20:01 +0530

    updated

---
+++ part3/page28a.js
@@ -0,0 +1,30 @@
+const products = [
+  { id: 1, name: "Product 1", price: 25 },
+  { id: 2, name: "Product 2", price: 50 },
+  { id: 3, name: "Product 3", price: 45 },
+];
+let cart = []
+//Display all products using forEach
+function displayProducts(){
+
+}
+
+//Add product 2 to cart using spread operator with a
+//new key quanity and assign 1
+function addToCart(id){
+
+}
+//display cart
+function displayCart(){
+
+}
+
+displayProducts()
+addToCart(2)
+displayCart()
+addToCart(1)
+displayCart()
+
+
+
+
