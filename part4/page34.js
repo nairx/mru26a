50687d7935c94da9b0f4e6ad6cf7bf2da2969f5c
nairx <nairxprv@gmail.com>
2026-03-06 11:59:45 +0530

    updated

--- part4/page34.js
+++ part4/page34.js
@@ -1,14 +1,39 @@
 import express from "express";
 const app = express();
-app.set("view engine","ejs")
-app.set("views","views")
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.listen(5000, () => console.log("Server Started"));
+
+app.use(express.urlencoded({ extended: true }));
+
+const users = [
+  { name: "Poojitha", email: "poojitha@gmail.com", password: "1234" },
+  { name: "Ajay", email: "ajay@gmail.com", password: "1234" },
+  { name: "Komal", email: "komal@gmail.com", password: "1234" },
+];
+
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    if (user.password === password) {
+      res.redirect("/");
+    } else {
+      res.redirect("/login");
+    }
+  } else {
+    res.redirect("/login");
+  }
+  res.redirect("/");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
 app.get("/", (req, res) => {
-  res.render("dashboard");
+  res.render("dashboard", { users });
 });
