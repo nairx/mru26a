cd919a402317cab09be05ebb630cf40e151a032b
nairx <nairxprv@gmail.com>
2026-03-06 14:47:15 +0530

    updated

--- part4/page34.js
+++ part4/page34.js
@@ -1,16 +1,35 @@
 import express from "express";
+import session from "express-session";
+import expressLayouts from "express-ejs-layouts";
 const app = express();
+app.use(expressLayouts)
+app.use(express.static("public"))
+app.set("layout","layout")
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.listen(5000, () => console.log("Server Started"));
-
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "mySecretKey", // recommended to store in env file
+    resave: false, // if true session is saved every request even if there is no change.
+    saveUninitialized: false, //if true a default cookie gets created immediately
+  }),
+);
+
+// app.use((req, res, next) => {
+//   req.locals.user = req.session.user;
+//   next();
+// });
 
 let users = [
   { name: "Poojitha", email: "poojitha@gmail.com", password: "1234" },
   { name: "Ajay", email: "ajay@gmail.com", password: "1234" },
   { name: "Komal", email: "komal@gmail.com", password: "1234" },
 ];
+
+
 
 app.get("/login", (req, res) => {
   res.render("login", { error: null });
@@ -21,6 +40,7 @@ app.post("/login", (req, res) => {
   const user = users.find((user) => user.email === email);
   if (user) {
     if (user.password === password) {
+      req.session.user = user; //session id gets created, check in browser, inspect - application - cookies
       res.redirect("/");
     } else {
       res.render("login", { error: "Invalid Password" });
@@ -28,7 +48,6 @@ app.post("/login", (req, res) => {
   } else {
     res.render("login", { error: "User not found" });
   }
-  // res.redirect("/");
 });
 
 app.get("/register", (req, res) => {
@@ -41,5 +60,9 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("dashboard", { users });
+  if (req.session.user) {
+    res.render("dashboard", { users });
+  } else {
+    res.redirect("/login");
+  }
 });
